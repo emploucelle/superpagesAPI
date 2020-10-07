c10e36989d0d02813eebd3331f5268fc456969e1
emploucelle <emploucelle@gmail.com>
2020-10-07 12:39:56 +0800

    update migration

--- database/migrations/2020_10_06_122352_create_states_table.php
+++ database/migrations/2020_10_06_122352_create_states_table.php
@@ -15,19 +15,19 @@ class CreateStatesTable extends Migration
     {
         Schema::create('states', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('CountryId ')->nullable()->default('');
+            $table->string('CountryId ')->nullable()->default('');
             $table->string('State')->nullable()->default('');
             $table->string('slug')->nullable()->default('');
             $table->string('Code')->nullable()->default('');
             $table->string('ADM1Code')->nullable()->default('');
-            $table->integer('status')->nullable()->default('');
+            $table->string('status')->nullable()->default('');
             $table->string('price')->nullable()->default('');
-            $table->integer('is_featured')->nullable()->default('');
+            $table->string('is_featured')->nullable()->default('');
             $table->string('seo_friendly_title')->nullable()->default('');
             $table->string('seo_meta_description')->nullable()->default('');
             $table->string('seo_meta_keyword')->nullable()->default('');
             $table->text('contents')->nullable()->default('');
-            $table->integer('view_flag')->nullable()->default('0');
+            $table->string('view_flag')->nullable()->default('0');
             $table->timestamps();
         });
     }
